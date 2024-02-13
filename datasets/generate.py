0f23cdb7b9634c6ab9462839803285c1a0003779
monthiller <monthiller.victor@hotmail.fr>
2024-02-13 14:07:17 +0100

    Create generate.py

---
+++ datasets/generate.py
@@ -0,0 +1,16 @@
+def light_on_after_12():
+  dataset = {
+    "inputs":[],
+    "outputs":[],
+    "tolerance":[],
+  }
+  for i in range(50):
+    input = 24*i/50
+    if input<=12:
+      dataset["inputs"].append([input,])
+      dataset["outputs"].append([0,])
+      dataset["tolerance"].append([0, 0.4])
+      continue
+    dataset["inputs"].append([input,])
+    dataset["outputs"].append([1])
+    dataset["tolerance"].append([0.6, 1])
