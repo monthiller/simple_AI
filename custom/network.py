f7959b6ea4ea26fe7d05fede3e6cc939e88b001c
monthiller <monthiller.victor@hotmail.fr>
2024-02-14 12:59:48 +0100

    Update network.py
    Automatic graph size.

--- custom/network.py
+++ custom/network.py
@@ -293,7 +293,7 @@ class Network:
         positions[name] = (position, i)
 
     # Draw the network
-    plt.figure(figsize=(12, 8))
+    # plt.figure(figsize=(12, 8))
     nx.draw(G, positions, with_labels=True, node_size=1500, node_color="skyblue", font_size=12, arrows=True)
     plt.title("Neural Network Architecture")
     plt.show()
