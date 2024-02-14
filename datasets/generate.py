f85820d14cc6c3a4839ebc6f2b4de2e84efb4a6b
monthiller <monthiller.victor@hotmail.fr>
2024-02-14 14:31:28 +0100

    Update generate.py
    Add dataset

--- datasets/generate.py
+++ datasets/generate.py
@@ -15,3 +15,21 @@ def light_on_after_12():
     dataset["outputs"].append([1])
     dataset["tolerance"].append([0.6, 1])
   return dataset
+
+def light_on_after_19():
+  dataset = {
+    "inputs":[],
+    "outputs":[],
+    "tolerance":[],
+  }
+  for i in range(50):
+    input = 24*i/50
+    if input<=19:
+      dataset["inputs"].append([input,])
+      dataset["outputs"].append([0,])
+      dataset["tolerance"].append([0, 0.4])
+      continue
+    dataset["inputs"].append([input,])
+    dataset["outputs"].append([1])
+    dataset["tolerance"].append([0.6, 1])
+  return dataset
